81c50248ac1843de3c06d6d21e8fc4672bfb726a
Michael <michaeli@touchnote.com>
2014-04-05 15:13:19 +0100

    initial implementation idea for backwards animation

--- InteractiveTransition/SCTransition.m
+++ InteractiveTransition/SCTransition.m
@@ -10,34 +10,25 @@
 
 
 @interface SCTransition ()
-@property (nonatomic, weak) UIViewController *fromViewController;
-@property (nonatomic, weak) UIViewController *toViewController;
-@property (nonatomic, weak) UIView *containerView;
-@property (nonatomic ) CGFloat completionSpeed;
-@property UIViewAnimationCurve completionCurve;
-@property (nonatomic, weak) CALayer *animLayer;
+@property (nonatomic) CGFloat completionSpeed;
 @end
 
 @implementation SCTransition {
     __weak id <UIViewControllerContextTransitioning> _transitionContext;
     CGFloat _pausedTime;
+    BOOL _wasCanceled;
 }
 
 - (void)animateTransition:(id<UIViewControllerContextTransitioning>)transitionContext {
-    //Get references to the view hierarchy
+
     UIView *containerView = [transitionContext containerView];
     UIViewController *fromViewController = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
     UIViewController *toViewController = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
-    
-    self.fromViewController = fromViewController;
-    self.toViewController = toViewController;
-    self.containerView = containerView;
     
     if (self.transitionDirection == kSCCardTransitionForwards) {
         UIView *view = [fromViewController valueForKeyPath:@"squareView"];
         [self animateLayer:view.layer withCompletion:^{
             [containerView insertSubview:toViewController.view aboveSubview:fromViewController.view];
-            [fromViewController.view removeFromSuperview];
             [transitionContext completeTransition:YES];
         }];
 
@@ -46,23 +37,47 @@
         [containerView insertSubview:toViewController.view aboveSubview:fromViewController.view];
         [fromViewController.view removeFromSuperview];
         UIView *view = [toViewController valueForKeyPath:@"squareView"];
-        [self animateLayer:view.layer withCompletion:^{ [transitionContext completeTransition:YES]; }];
+        [self animateLayer:view.layer withCompletion:^{
+            NSLog(@"complete %@", _wasCanceled ? @"caneled" : @"finished");
+            [transitionContext completeTransition:!_wasCanceled];
+            if (_wasCanceled) {
+                [containerView addSubview:fromViewController.view];
+                [fromViewController.view removeFromSuperview];
+            }
+        }];
     }
 }
 
 - (NSTimeInterval)transitionDuration:(id<UIViewControllerContextTransitioning>)transitionContext {
     return 1.0;
 }
+
 - (void)startInteractiveTransition:(id<UIViewControllerContextTransitioning>)transitionContext {
     _transitionContext = transitionContext;
     [self animateTransition:_transitionContext];
-    self.animLayer = self.containerView.layer;
-    [self pauseLayer:self.animLayer];
+    [self pauseLayer:[transitionContext containerView].layer];
 
 }
+
 - (void)updateInteractiveTransition:(CGFloat)percentComplete {
-    self.animLayer.timeOffset =  _pausedTime + [self transitionDuration:_transitionContext]*percentComplete;
+    [_transitionContext containerView].layer.timeOffset =  _pausedTime + [self transitionDuration:_transitionContext]*percentComplete;
     [_transitionContext updateInteractiveTransition:percentComplete];
+}
+
+- (void)cancelInteractiveTransition {
+    _wasCanceled = YES;
+    CALayer *containerLayer =[_transitionContext containerView].layer;
+    self.completionSpeed = (containerLayer.timeOffset - _pausedTime);
+    [_transitionContext cancelInteractiveTransition];
+    [self resumeLayer:containerLayer];
+    containerLayer.speed = 1.0;
+}
+
+- (void)finishInteractiveTransition {
+    _wasCanceled = NO;
+    self.completionSpeed = [_transitionContext containerView].layer.timeOffset - _pausedTime;
+    [_transitionContext finishInteractiveTransition];
+    [self resumeLayer:[_transitionContext containerView].layer];
 }
 
 - (void)handleGesture:(UIScreenEdgePanGestureRecognizer *)recognizer {
@@ -76,11 +91,9 @@
         case UIGestureRecognizerStateCancelled:
         case UIGestureRecognizerStateEnded:{
             if (progress < 0.5){
-                [_transitionContext completeTransition:NO];
+                [self cancelInteractiveTransition];
             }else{
-                self.completionSpeed =  [self transitionDuration:_transitionContext]*progress;
-                [_transitionContext finishInteractiveTransition];
-                [self resumeLayer:self.animLayer];
+                [self finishInteractiveTransition];
             }
             self.shouldBeginInteractiveTransition = NO;
             break;
@@ -89,16 +102,15 @@
             break;
     }
 }
--(void)pauseLayer:(CALayer*)layer
-{
+
+- (void)pauseLayer:(CALayer*)layer {
     CFTimeInterval pausedTime = [layer convertTime:CACurrentMediaTime() fromLayer:nil];
     layer.speed = 0.0;
     layer.timeOffset = pausedTime;
     _pausedTime = pausedTime;
 }
 
--(void)resumeLayer:(CALayer*)layer
-{
+- (void)resumeLayer:(CALayer*)layer {
     CFTimeInterval pausedTime = [layer timeOffset];
     layer.speed = 1.0;
     layer.timeOffset = 0.0;
@@ -106,6 +118,7 @@
     CFTimeInterval timeSincePause = [layer convertTime:CACurrentMediaTime() fromLayer:nil] - pausedTime;
     layer.beginTime = timeSincePause;
 }
+
 - (void)animateLayer:(CALayer *)layer withCompletion:(void(^)())block {
     
     CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.y"];
