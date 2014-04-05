e0f5fdca9c67986f6e2bf26215ba76d07820c3f7
Michael <michaeli@touchnote.com>
2014-04-05 13:55:43 +0100

    added call to context at update and finish interaction witch fixed the problem

--- InteractiveTransition/SCTransition.m
+++ InteractiveTransition/SCTransition.m
@@ -12,16 +12,18 @@
 @interface SCTransition ()
 @property (nonatomic, weak) UIViewController *fromViewController;
 @property (nonatomic, weak) UIViewController *toViewController;
+@property (nonatomic, weak) UIView *containerView;
 @property (nonatomic ) CGFloat completionSpeed;
 @property UIViewAnimationCurve completionCurve;
+@property (nonatomic, weak) CALayer *animLayer;
 @end
 
 @implementation SCTransition {
     __weak id <UIViewControllerContextTransitioning> _transitionContext;
+    CGFloat _pausedTime;
 }
 
 - (void)animateTransition:(id<UIViewControllerContextTransitioning>)transitionContext {
-    NSLog(@"Are we no longer calling this");
     //Get references to the view hierarchy
     UIView *containerView = [transitionContext containerView];
     UIViewController *fromViewController = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
@@ -29,46 +31,89 @@
     
     self.fromViewController = fromViewController;
     self.toViewController = toViewController;
-    
+    self.containerView = containerView;
     
     if (self.transitionDirection == kSCCardTransitionForwards) {
-        UIView *view = [fromViewController valueForKeyPath:@"squareView"];
-        
-        void(^block)() = ^{
-            [containerView insertSubview:toViewController.view aboveSubview:fromViewController.view];
-            [fromViewController.view removeFromSuperview];
-            [transitionContext completeTransition:YES];
-        };
-        
-        if (USECA)
-            [self animateLayer:view.layer withCompletion:block];
-        else
-            [UIView animateWithDuration:[self transitionDuration:transitionContext] animations:^{
-                view.layer.transform = CATransform3DMakeRotation(M_PI, 0.0, 1.0, 0.0);
-            } completion:^(BOOL finished) { block();}];
-        
-    } else if (self.transitionDirection == kSCCardTransitionBackwards) {
-        [containerView insertSubview:toViewController.view aboveSubview:fromViewController.view];
-        [fromViewController.view removeFromSuperview];
-        UIView *view = [toViewController valueForKeyPath:@"squareView"];
-        
-        if (USECA)
-            [self animateLayer:view.layer withCompletion:^{
-                NSLog(@"completion");
-                [transitionContext completeTransition:YES];
-            }];
-        else
-        [UIView animateWithDuration:[self transitionDuration:transitionContext] animations:^{
-            view.layer.transform = CATransform3DIdentity;
-        } completion:^(BOOL finished) {
-            NSLog(@"completion");
-            [transitionContext completeTransition:YES];
-        }];
 
+        [containerView insertSubview:toViewController.view aboveSubview:fromViewController.view];
+        toViewController.view.transform = CGAffineTransformMakeTranslation(0.0, toViewController.view.bounds.size.height);
+        [UIView animateWithDuration:[self transitionDuration:transitionContext]
+                         animations:^{
+                             toViewController.view.transform = CGAffineTransformIdentity;
+                         }
+                         completion:^(BOOL finished) {
+                             [transitionContext completeTransition:YES];
+                         }];
+
+    } else if (self.transitionDirection == kSCCardTransitionBackwards) {
+        [containerView insertSubview:toViewController.view belowSubview:fromViewController.view];
+        [UIView animateWithDuration:[self transitionDuration:transitionContext]
+                         animations:^{
+                             fromViewController.view.transform = CGAffineTransformMakeTranslation(0.0, fromViewController.view.bounds.size.height);
+                         }
+                         completion:^(BOOL finished) {
+                             NSLog(@"completed");
+                            [_transitionContext completeTransition:YES];
+                         }];
     }
 }
 
+- (NSTimeInterval)transitionDuration:(id<UIViewControllerContextTransitioning>)transitionContext {
+    return 1.0;
+}
+- (void)startInteractiveTransition:(id<UIViewControllerContextTransitioning>)transitionContext {
+    _transitionContext = transitionContext;
+    [self animateTransition:_transitionContext];
+    self.animLayer = self.containerView.layer;
+    [self pauseLayer:self.animLayer];
 
+}
+- (void)updateInteractiveTransition:(CGFloat)percentComplete {
+    self.animLayer.timeOffset =  _pausedTime + [self transitionDuration:_transitionContext]*percentComplete;
+    [_transitionContext updateInteractiveTransition:percentComplete];
+}
+
+- (void)handleGesture:(UIScreenEdgePanGestureRecognizer *)recognizer {
+    CGFloat progress = [recognizer translationInView:recognizer.view].x / (recognizer.view.bounds.size.width * 1.0);
+    progress = MIN(1.0, MAX(0.0, progress));
+    
+    switch (recognizer.state) {
+        case UIGestureRecognizerStateChanged:
+            [self updateInteractiveTransition:progress];
+            break;
+        case UIGestureRecognizerStateCancelled:
+        case UIGestureRecognizerStateEnded:{
+            if (progress < 0.5){
+                [_transitionContext completeTransition:NO];
+            }else{
+                self.completionSpeed =  [self transitionDuration:_transitionContext]*progress;
+                [_transitionContext finishInteractiveTransition];
+                [self resumeLayer:self.animLayer];
+            }
+            self.shouldBeginInteractiveTransition = NO;
+            break;
+        }
+        default:
+            break;
+    }
+}
+-(void)pauseLayer:(CALayer*)layer
+{
+    CFTimeInterval pausedTime = [layer convertTime:CACurrentMediaTime() fromLayer:nil];
+    layer.speed = 0.0;
+    layer.timeOffset = pausedTime;
+    _pausedTime = pausedTime;
+}
+
+-(void)resumeLayer:(CALayer*)layer
+{
+    CFTimeInterval pausedTime = [layer timeOffset];
+    layer.speed = 1.0;
+    layer.timeOffset = 0.0;
+    layer.beginTime = 0.0;
+    CFTimeInterval timeSincePause = [layer convertTime:CACurrentMediaTime() fromLayer:nil] - pausedTime;
+    layer.beginTime = timeSincePause;
+}
 - (void)animateLayer:(CALayer *)layer withCompletion:(void(^)())block {
     
     CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.y"];
@@ -89,54 +134,4 @@
     }
 }
 
-- (NSTimeInterval)transitionDuration:(id<UIViewControllerContextTransitioning>)transitionContext {
-    return 2.0;
-}
-- (void)startInteractiveTransition:(id<UIViewControllerContextTransitioning>)transitionContext {
-    _transitionContext = transitionContext;
-    [self animateTransition:_transitionContext];
-//    UIView *view = [self.toViewController valueForKeyPath:@"squareView"];
-//    view.layer.speed = 0.0;
-}
-- (void)updateInteractiveTransition:(CGFloat)percentComplete {
-//    UIView *view = [self.toViewController valueForKeyPath:@"squareView"];
-//    view.layer.timeOffset = percentComplete;
-    NSLog(@"%f",percentComplete);
-}
-
-
-- (void)handleGesture:(UIScreenEdgePanGestureRecognizer *)recognizer {
-    CGFloat progress = [recognizer translationInView:recognizer.view].x / (recognizer.view.bounds.size.width * 1.0);
-    progress = MIN(1.0, MAX(0.0, progress));
-    
-    switch (recognizer.state) {
-        case UIGestureRecognizerStateChanged:
-            [self updateInteractiveTransition:progress];
-            break;
-        case UIGestureRecognizerStateCancelled:
-        case UIGestureRecognizerStateEnded:{
-            UIView *view = [self.toViewController valueForKeyPath:@"squareView"];
-            view.layer.timeOffset = [view.layer convertTime:progress toLayer:view.layer];
-            view.layer.beginTime = CACurrentMediaTime();
-            view.layer.speed = 1.0;
-
-            if (progress < 0.5){
-                //[_transitionContext completeTransition:NO];
-            }else{
-                //[_transitionContext completeTransition:YES];
-                //UIView *view = [self.toViewController valueForKeyPath:@"squareView"];
-                //view.layer.speed = 1.0;
-                //view.layer.beginTime = [view.layer convertTime:CACurrentMediaTime() fromLayer:nil];
-                [recognizer.view removeGestureRecognizer:recognizer];
-                self.completionSpeed = 1.0;
-                self.completionCurve = UIViewAnimationCurveEaseInOut;
-                
-            }
-            self.shouldBeginInteractiveTransition = NO;
-            break;
-        }
-        default:
-            break;
-    }
-}
 @end
--- InteractiveTransition/SCViewController.m
+++ InteractiveTransition/SCViewController.m
@@ -21,7 +21,11 @@
     CATransform3D transfrom = CATransform3DIdentity;
     transfrom.m34 = -1.f/500.f;
     self.view.layer.sublayerTransform = transfrom;
-    NSLog(@"one appered");
+    NSLog(@"%@",self.navigationController.viewControllers);
+
+}
+- (IBAction)buttonPressed:(id)sender {
+    NSLog(@"buttonPressed");
 }
 
 - (void)animateLayer:(CALayer *)layer withCompletion:(void(^)())block {
